13e1302e06b82aa1c78851bb7c69ce2896365112
Matheus Quirino <contato.matheuspquirino@gmail.com>
2025-09-23 10:47:43 -0300

    Project created

---
+++ src/main/java/com/mpq/projetowebservices/ProjetowebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mpq.projetowebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetowebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetowebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpq/projetowebservices/ProjetowebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpq.projetowebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetowebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
