55f1524c7b9fb313927a1282753c7debd6b484f0
Matheus Quirino <contato.matheuspquirino@gmail.com>
2025-10-02 10:35:35 -0300

    Add: Payment, One-to-one association

--- src/main/java/com/mpq/projetowebservices/config/TestConfig.java
+++ src/main/java/com/mpq/projetowebservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.mpq.projetowebservices.entities.Category;
 import com.mpq.projetowebservices.entities.Order;
 import com.mpq.projetowebservices.entities.OrderItem;
+import com.mpq.projetowebservices.entities.Payment;
 import com.mpq.projetowebservices.entities.Product;
 import com.mpq.projetowebservices.entities.User;
 import com.mpq.projetowebservices.entities.enums.OrderStatus;
@@ -77,6 +78,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
