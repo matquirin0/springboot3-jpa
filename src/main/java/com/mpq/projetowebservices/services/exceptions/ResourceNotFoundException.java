16c23f987c8c36518094dd78a16de73532b7dad1
Matheus Quirino <contato.matheuspquirino@gmail.com>
2025-10-02 13:25:52 -0300

    Add: Exception handling - findById

---
+++ src/main/java/com/mpq/projetowebservices/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.mpq.projetowebservices.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+	
+}
